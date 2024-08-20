db5fd47a8f14122f0acd3b3238d1ad3caf7e2a55
Jorge_Vicharra <I202406654@cibertec.edu.pe>
2024-08-20 00:57:04 +0000

    he creado el componente Titulo

---
+++ WebTest/src/componentes/titulo.jsx
@@ -0,0 +1,13 @@
+import {Fragment} from "react";
+
+function titulo(){
+
+    return(
+        <>
+        <h1>Conoce el Peru primero</h1>
+        <h2>En el Peru existen hermosos lugares para conocer</h2>
+        </>
+    );
+}
+
+export default titulo;
