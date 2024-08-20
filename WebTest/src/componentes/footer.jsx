ae4807f89ff0e160be5c2beb06e7c69310489b2c
Jorge_Vicharra <I202406654@cibertec.edu.pe>
2024-08-20 01:05:07 +0000

    creacion del footer

---
+++ WebTest/src/componentes/footer.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function footer(){
+
+    return (
+        <>
+        <h4>TravelFast</h4>
+        <h5>Expertos en recorrer el PEru</h5>
+        </>
+    );
+}
+
+export default footer;
+
