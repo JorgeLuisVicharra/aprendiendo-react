0cf27e4dbbb1ef1927c5e5c53ae2ef8b057cc40d
Jorge_Vicharra <I202406654@cibertec.edu.pe>
2024-08-20 01:15:26 +0000

    agregando estilos personalizados

--- WebTest/src/App.jsx
+++ WebTest/src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
-import titulo from './componentes/Titulo';
+import titulo from './componentes/titulo';
+import footer from './componentes/footer';
 
 function App (){
 
     return (
       <>
       <titulo/>
+      <footer/>
       </>
     );
 }
