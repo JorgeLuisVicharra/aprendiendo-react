a78d22eed00c72bf80a3ceedd17f79a7815ba49b
Jorge_Vicharra <I202406654@cibertec.edu.pe>
2024-08-25 01:16:41 +0000

    modificando algunos componentes y primer error XD

--- Proyect-DEW/src/App.jsx
+++ Proyect-DEW/src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import { Fragment } from 'react'
 import navbar from './componentes/navbar';
 import carrusel from './componentes/carrusel';
-import cards from './componentes/cards';
+import cards from './componentes/cards'
 import acordeon from './componentes/acordeon';
 
 function App(){
   return(
     <>
       <navbar/>
+      <br></br>
       <carrusel/>
+      <br></br>
       <cards/>
-      <acordeon/>
-      
-      </>
+      <br></br>
+      <acordeon/>      
+    </>
   );
 }
 
